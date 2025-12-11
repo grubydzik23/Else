3aafec6f678c9c017f43df375ebc724eaf759cfb
grubydzik23 <anthad2004@gmail.com>
2025-12-11 09:45:00 +0100

    Pierwszy commit

---
+++ Firma transportowa/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace FirmaTransportowa
+{
+    enum StatusPojazdu
+    {
+        Dostepny,
+        WTrasie,
+        Serwis
+    }
+    class Pojazd
+    {
+        public string Marka { get; set; }
+        public int RokProdukcji { get; set; }
+        public DateTime WaznoscBadaniaTechnicznego { get; set; } 
+        public StatusPojazdu Status { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            private void ZarzadzaniePojazdami()
+                while(true)
+                    Console.WriteLine("1. Rejestracja pojazdu");
+            {
+                Console.WriteLine("1. Zarzadanie pojazdami");
+                var choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        ZarzadaniePojazdami();
+                        break;
+                }
+            }
+        }
+    }
+}
